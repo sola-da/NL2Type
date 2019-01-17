529b44c441135fff155fe3cd20639b061cd0646d
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-17 21:24:33 +0100

    Added script for downloading data from gdrive

---
+++ scripts/download_data.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+import sys
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print "Incorrect number of args. Expecting one arg"
+        exit(1)
+
+    with(open(sys.argv[1])) as f:
+        configs = json.loads(f.read())
+        print arr
+
+    for conf in configs:
+        download_file_from_google_drive(conf['file_id'], conf['destination'])
