5f0aaee6cb1d27ff03d238284768ea76ef99cf73
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-19 16:22:19 +0100

    Added code for reproducing top k results and for function and param specific results

--- scripts/enrich_results.py
+++ scripts/enrich_results.py
@@ -4,3 +4,8 @@ def invoke(config):
     print "Enriching results"
     df_results = pd.read_csv(config['input_file_path_results'])
     df_data = pd.read_csv(config['input_file_path_data'])
+
+    df_results['datapoint_type'] = df_data['datapoint_type']
+    df_results['filename'] = df_data['filename']
+
+    df_results.to_csv(config['output_file_path'])
--- scripts/stats.py
+++ scripts/stats.py
@@ -5,6 +5,12 @@ import pandas as pd
 def invoke(config):
     results_df = pd.read_csv(config['input_file_path'])
     calculate_f1_scores(results_df)
+    print ""
+    calculate_top_f1(results_df, 3)
+    print ""
+    calculate_top_f1(results_df, 5)
+    print ""
+    calculate_f1_scores_param_func(results_df)
 
 
 def calculate_f1_scores(df):
@@ -23,3 +29,65 @@ def calculate_f1_scores(df):
     recall = correct / total
     f1 = 2 * ((precision * recall) / (precision + recall))
     print "precision: {}, recall, {}, f1: {}".format(precision, recall, f1)
+
+
+def calculate_top_f1(df, k):
+    print "Calculating f1 scores for top {}".format(k)
+    correct = 0
+    total = 0
+    # top_k_corpus = ['string', 'number', 'object', 'boolean', 'function']
+    for index, row in df.iterrows():
+        if row['prediction_string'] == 'other':
+            continue
+        top_k = row['top_k'].split("%")[:k]
+        # top_k = top_k_corpus[:k]
+        if row['original'] in top_k:
+            correct += 1
+        total += 1
+
+
+    print "correct: ", correct
+    print "total predictions: ", total
+    print "total data points: ", df.shape[0]
+    precision = correct/total
+    recall = correct/df.shape[0]
+    f1 = 2 * ((precision * recall) / (precision + recall))
+    print "Precision top {}: {}".format(k, correct/total)
+    print "Recall top {}: {}".format(k, correct/df.shape[0])
+    print "F1 top{}: {}".format(k, f1)
+
+
+def calculate_f1_scores_param_func(df):
+    correct = 0
+    predictions = 0
+    total = df[df['datapoint_type'] == 0].shape[0]
+    print "Total number of functions is: ", total
+    for index, row in df.iterrows():
+        if df.loc[index, 'datapoint_type'] == 1:
+            continue
+        if row['prediction_string'] != 'other':
+            predictions += 1
+            if row['original'] == row['prediction_string']:
+                correct += 1
+
+    precision = correct / predictions
+    recall = correct / total
+    f1 = 2 * ((precision * recall) / (precision + recall))
+    print "precision of function: {}, recall, {}, f1: {}".format(precision, recall, f1)
+
+    correct = 0
+    predictions = 0
+    total = df[df['datapoint_type'] == 1].shape[0]
+    print "Total number of params is: ", total
+    for index, row in df.iterrows():
+        if df.loc[index, 'datapoint_type'] == 0:
+            continue
+        if row['prediction_string'] != 'other':
+            predictions += 1
+            if row['original'] == row['prediction_string']:
+                correct += 1
+
+    precision = correct / predictions
+    recall = correct / total
+    f1 = 2 * ((precision * recall) / (precision + recall))
+    print "precision of param: {}, recall, {}, f1: {}".format(precision, recall, f1)
