61b0de8c57c31f7076e7c34d81981e51d2af24d6
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-23 23:43:44 +0100

    Fixed a small bug in init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,4 +6,4 @@ mkdir ../data/demo/jsdoc_out
 mkdir ../data/demo/raw_csv
 mkdir ../data/demo/vecs
 
-unzip ../data/intermediate_data.zip -d ../data/paper
+unzip ../intermediate_data.zip -d ../data/paper
