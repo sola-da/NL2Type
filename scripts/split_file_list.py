a439a8b2d3215fc430525268629dedc1decd7de9
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-26 13:57:52 +0100

    Added script for splitting list

---
+++ scripts/split_file_list.py
@@ -0,0 +1,17 @@
+import os
+
+def invoke(config):
+    with open(config['input_file']) as files:
+        files_list = files.read().splitlines()
+
+    files_training = files_list[:int(len(files) * .8)]
+    files_testing = files[int(len(files) * .8):]
+
+    with open(os.path.join(config['output_dir'], "training.txt"), 'w') as f:
+        for f_t in files_training:
+            f.write(f_t + "\n")
+
+    with open(os.path.join(config['output_dir'], "testing.txt"), 'w') as f:
+        for f_t in files_testing:
+            f.write(f_t + "\n")
+
