7debea0ae173c1e533e2baa7c97fbad9112ad1c4
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-26 13:18:43 +0100

    File splitter outputs file now

--- scripts/generate_file_split_list.py
+++ scripts/generate_file_split_list.py
@@ -4,18 +4,15 @@ from random import shuffle
 
 def invoke(config):
     files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(config['input_dir']) for f in filenames if f.endswith(config['extension'])]
-    print len(files)
     shuffle(files)
-    files_training = files[:int(len(files) * .8)]
-    files_testing = files[int(len(files) * .8):]
-
-    print len(files_training)
-    print len(files_testing)
-    #print files
-    # with open(os.path.join(config['output_dir'], "training.txt"), 'w') as f:
-    #     for f_t in files_training:
-    #         f.write(f_t + "\n")
+    # files_training = files[:int(len(files) * .8)]
+    # files_testing = files[int(len(files) * .8):]
     #
+
+    with open(os.path.join(config['output_dir'], "files.txt"), 'w') as f:
+        for f_t in files:
+            f.write(f_t + "\n")
+
     # with open(os.path.join(config['output_dir'], "testing.txt"), 'w') as f:
     #     for f_t in files_testing:
     #         f.write(f_t + "\n")
