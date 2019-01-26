e3f65c0386c37e2f519bb4f271932043594dc572
Rabee Sohail Malik <rabeesohail.malik@stud.tu-darmstadt.de>
2019-01-26 13:59:13 +0100

    Fixed bug in split list script

--- scripts/split_file_list.py
+++ scripts/split_file_list.py
@@ -4,8 +4,8 @@ def invoke(config):
     with open(config['input_file']) as files:
         files_list = files.read().splitlines()
 
-    files_training = files_list[:int(len(files) * .8)]
-    files_testing = files[int(len(files) * .8):]
+    files_training = files_list[:int(len(files_list) * .8)]
+    files_testing = files[int(len(files_list) * .8):]
 
     with open(os.path.join(config['output_dir'], "training.txt"), 'w') as f:
         for f_t in files_training:
